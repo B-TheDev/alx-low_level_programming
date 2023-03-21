a1957709b3277457539d35d8c0e7ad728c361c24
Buhle Seya <buhleseya@yahoo.com>
2023-03-21 00:06:25 -0700

    4. Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-
+#include<stdio.h>
+/**
+ * main - A program using the puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like holding a multilingual puzzle");
+	return(0);
+}
