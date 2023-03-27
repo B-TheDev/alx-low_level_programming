49fb13aabfc32487f94ae2587d3257a8bbbcda2c
Buhle Seya <buhleseya@yahoo.com>
2023-03-27 15:38:02 -0700

    0. 98 Battery Str

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
 Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
@@ -7,6 +7,5 @@ Write a function that takes a pointer to an int as parameter and updates the val
 
 void reset_to_98(int *n)
 {
-
 *n =98;
 }
