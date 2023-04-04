2819aba5513ce172cc6b3fb75080bd130df1a726
Buhle Seya <buhleseya@yahoo.com>
2023-04-04 16:42:38 -0700

    6. Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - function finds the first occurrence of the substring needle in the string haystack. The terminating null bytes (\0) are not compared
+ * @haystack:
+ * @needle:
+ * Return:  pointer to the beginning of the located substring, or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for(;*haystack != '\0'; haystack++)
+	{
+char *a = haystack;
+char *b = needle;
+
+while (*a == *b && *b != '\0')
+	{
+	a++;
+	b++;
+	}
+if (*b == '\0')
+	return (haystack);
+	}
+	return(0);
+}
