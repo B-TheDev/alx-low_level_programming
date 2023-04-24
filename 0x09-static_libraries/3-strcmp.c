819d70059ff14179f904b19d3a0c87e74db03975
Buhle Seya <buhleseya@yahoo.com>
2023-04-24 02:15:39 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+The comments section. 
+
+The function declaration follo after the main block.
+
+*/
+
+int main(void)
+{
+
+printf();
+return 0;
+}
+
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+The comments section. 
+
+The function declaration follo after the main block.
+
+*/
+
+int main(void)
+{
+
+printf();
+return 0;
+}
