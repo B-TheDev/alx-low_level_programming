8fd023e1ed78c2b58f78e6645763ade645988407
B Seya <buhleseya@yahoo.com>
2023-07-18 14:45:47 -0700

    Updating repository

---
+++ 0x01-variables_if_else_while/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, 
+ * in lowercase, followed by a new line.
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
