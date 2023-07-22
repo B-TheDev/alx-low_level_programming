b399bb8874708b1de852dfd7e265725199e252f3
B Seya <buhleseya@yahoo.com>
2023-07-22 09:59:48 -0700

    Updating repository

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
 #include "main.h"
-	
+
 /**
- * _atoi- Converts a string into an int.
- * @s: The string to be converted.
+ * _atoi - converts a string to an integer
+ * @s: The string to be converted
  *
- * Return: 0 (Success)
+ * Return: the int converted from the string
  */
 
 int _atoi(char *s)
 {
-	int i, d, f, n, len, digit;
+	int i, d, n, len, f, digit = 0;
 
-	i = 0;
-
-	d = 0; 
-
-	f = 0; 
-
-	len = 0;
-
-	digit = 0;
-
-	while (s[len] != '\0')
-	{
-		++len;
+		while (s[len] != '\0')
+		len++;
 
 	while (i < len && f == 0)
 	{
@@ -32,43 +21,32 @@ int _atoi(char *s)
 		{
 			++d;
 		}
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-		}
-		if (d % 2)
-		{
+			if (d % 2)
 			digit = -digit;
-
-		n = n * 10 + digit;
-
-		f = 1;
-		}
-
-		if (s[i + 1] < '0' || s[i + 1] > '9')
-		{
-			break;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
 			f = 0;
-			++i;
 		}
-
+		i++;
+	}
 		if (f == 0)
-		{
-			return (0);
-		}
-	return (n);
+		return (0);
 
+	return (n);
 }
-}
-}
+
+
 /**
-		
- * main - A program that multiples two  numbers.
- * @argc: The number of arguments
- * @argv: The one dimensional array of arguments
+ * main - a program that multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: 0(Success), 1 (Error)
+ * Return: 0 (Success), 1 (Error)
  */
 
 int main(int argc, char *argv[])
@@ -80,14 +58,9 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
 	num1 = _atoi(argv[1]);
-
 	num2 = _atoi(argv[2]);
-
 	result = num1 * num2;
-
 	printf("%d\n", result);
-
 	return (0);
 }
