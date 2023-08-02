3a08735d51907cb025b7c1ef0692186d3c431ad6
B Seya <buhleseya@yahoo.com>
2023-08-02 05:07:11 -0700

    Updating repo

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -10,4 +11,7 @@ int _sqrt_recursion(int n);
 int sqrt_r(int n, int i);
 int is_prime_number(int n);
 int _prime(int n, int i);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
 #endif
