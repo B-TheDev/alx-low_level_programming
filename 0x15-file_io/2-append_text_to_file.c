386cc3b4f337c4dd49911dd8bbfc038d51f2f63c
B Seya <buhleseya@yahoo.com>
2023-08-09 11:18:40 -0700

    Updating repo

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,30 +11,30 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int o, w, len = 0;
+	int o, w, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+	return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            ++len;
-    }
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+		++len;
+	}
 
-    o = open(filename, O_WRONLY | O_APPEND);
-    if (o == -1)
-        return (-1);
+	o = open(filename, O_WRONLY | O_APPEND);
+	if (o == -1)
+	return (-1);
 
-    w = write(o, text_content, len);
-    if (w == -1)
-    {
-        close(o);
-        return (-1);
-    }
+	w = write(o, text_content, len);
+	if (w == -1)
+	{
+		close(o);
+		return (-1);
+	}
 
-    close(o);
+	close(o);
 
-    return (1);
+	return (1);
 }
 
