9628abedaabb3df9a35a4adba93ba03105573d76
B Seya <buhleseya@yahoo.com>
2023-08-09 09:29:19 -0700

    Updating repository

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,54 @@
 #include "main.h"
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX
- * standard output.
+ * read_textfile - Reads a text file and prints it
+ * to the POSIX standard output.
  * @filename: Name of the file to be read.
  * @letters: Number of letters to be read and printed.
  *
- * Return: The actual number of bytes read and printed, 0 on failure or if filename is NULL.
+ * Return: The actual number of bytes read and printed, 0
+ * on failure or if filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buf;
-    ssize_t fd, t, w;
+	char *buf;
+	ssize_t fd, w, t;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buf = malloc(sizeof(char) * letters);
-    if (buf == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    t = read(fd, buf, letters);
-    if (t == -1)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
+	t = read(fd, buf, letters);
+	if (t == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
-    w = write(STDOUT_FILENO, buf, t);
-    if (w == -1)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
+	w = write(STDOUT_FILENO, buf, t);
+	if (w == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
-    free(buf);
-    close(fd);
-    return (w);
+	free(buf);
+	close(fd);
+	return (w);
 }
+
